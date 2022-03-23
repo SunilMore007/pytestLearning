d272a8ac55ffc34874e62ad8871d0098743a4456
sunil.more <sunilmore0057@gmail.com>
2022-03-23 11:00:22 +0530

    Added New Test cases

---
+++ AutomationCases/test_TC_003_Third_Test_Case.py
@@ -0,0 +1,10 @@
+import pytest
+
+
+# Test case code must be written inside the method name must be started with test
+# to run test case command: pytest testcase/method name
+
+
+@pytest.mark.TopPriority
+def test_tc_003_Registration_Testing():
+    print("this is Smoke test case code")
---
+++ AutomationCases/test_TC_004_Fourth_Test_Case.py
@@ -0,0 +1,11 @@
+import pytest
+
+# Test case code must be written inside the method name must be started with test
+# to run test case command: pytest testcase/method name
+
+
+@pytest.mark.TopPriority
+def test_tc_004_Invalid_login_logout_testing():
+    print("this is Sanity test case code")
+
+
